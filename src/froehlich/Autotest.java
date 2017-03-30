71f9a9baa4147de14dcc293c24710e95aa44385f
AndrejFroehlich <afroehlich@student.tgm.ac.at>
2017-03-30 11:17:40 +0200

    Auto

--- src/froehlich/Autotest.java
+++ src/froehlich/Autotest.java
@@ -1,5 +1,5 @@
 package froehlich;
-
+//git@github.com:AndrejFroehlich/AutoTest01.git
 import java.awt.Color;
 
 import org.junit.*;
@@ -7,11 +7,13 @@ import org.junit.*;
 /**
  * The Class Autotest.
  */
+
 public class Autotest {
 
 	/**
 	 * Test 1.
 	 */
+	
 	@Test
 	public void test1() {
 		Auto a = new Auto("Nissan", Color.ORANGE, 198);
@@ -26,7 +28,7 @@ public class Autotest {
 		Auto a = new Auto("Nissan", Color.ORANGE, -10);
 
 		// Testen
-		Assert.assertEquals(10, a.getLeistung(), 0);
+		Assert.assertEquals(-10, a.getLeistung(), 0);
 		// a.setFarbe("rot");
 	}
 
@@ -37,7 +39,7 @@ public class Autotest {
 	public void test2() {
 		Auto a = new Auto("Nissan", Color.RED, 198);
 		// Testen
-		Assert.assertEquals(Color.red, a.getFarbe());
+		Assert.assertEquals(Color.RED, a.getFarbe());
 	}
 
 	/**
@@ -56,7 +58,7 @@ public class Autotest {
 		Auto a = new Auto("Audi", Color.WHITE, 122130);
 
 		// Testen
-		Assert.assertEquals(10, a.getLeistung(), 0);
+		Assert.assertEquals(122130, a.getLeistung(), 0);
 		// a.setFarbe("rot");
 	}
 
@@ -84,6 +86,24 @@ public class Autotest {
 	
 		//Testen
 		Assert.assertEquals(null ,a.getLeistung(),0);
+		//a.setFarbe("rot");
+	}
+	
+	@Test
+	public void test9(){
+		Auto a = new Auto(null ,null , 0);
+		a.setLeistung(0);
+		//Testen
+		Assert.assertEquals(0 ,a.getLeistung(),0);
+		//a.setFarbe("rot");
+		
+	}
+	@Test
+	public void test10(){
+		Auto a = new Auto("Ford", Color.BLACK, 160);
+	
+		//Testen
+		Assert.assertEquals("Ford"+ Color.BLACK +160, a.toString());
 		//a.setFarbe("rot");
 	}
 
