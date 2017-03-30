12bb96b7f9a5d0ddafe21753e5f6b5e2b0691c6c
AndrejFroehlich <afroehlich@student.tgm.ac.at>
2017-03-30 11:00:03 +0200

    Autotest

--- src/froehlich/Auto.java
+++ src/froehlich/Auto.java
@@ -1,49 +1,85 @@
 package froehlich;
+import java.awt.Color;
 
+/**
+ * Eine einfache Klasse aus 3 Attributen, zugehörige Konstruktoren, set- und get-Methoden 
+ * und einer toString-Methode. 
+ * @author Johann Weiser
+ * @version 2016-02-06
+ */
 public class Auto {
-	private String farbe;
-	private int ps;
-	private String model;
-
-	public Auto(String farbe, int ps, String model) {
-		this.farbe = farbe;
-		this.ps = ps;
-		this.model = model;
+	private String type;
+	private Color farbe;
+	private int leistung;
+	
+	public Auto() {
+		type ="Ford";
+		farbe = Color.RED;
+		leistung = 160;
 	}
 
-	public Auto(){
-		this.farbe= "Violett";
-		this.ps = 156;
-		this.model = "Nissan GTR";
+	public Auto(String type, Color farbe, int leistung) {
+		if (type == null || farbe == null || leistung <=0) {
+			throw new IllegalArgumentException();
+		}
+		if (type.trim().length()==0) {
+			throw new IllegalArgumentException();
+		}
+		this.type = type;
+		this.farbe = farbe;
+		this.leistung = leistung;
 	}
 	
-	public String getFarbe() {
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		if (type.trim().length()==0) {
+			throw new IllegalArgumentException();
+		}
+		this.type = type;
+	}
+
+	public Color getFarbe() {
 		return farbe;
 	}
 
-	public void setFarbe(String farbe) {
+	public void setFarbe(Color farbe) {
+		if (farbe==null) {
+			throw new IllegalArgumentException();
+		}
 		this.farbe = farbe;
 	}
 
-	public int getPs() {
-		return ps;
+	public int getLeistung() {
+		return leistung;
 	}
 
-	public void setPs(int ps) {
-		this.ps = ps;
+	public void setLeistung(int leistung) {
+		if (leistung<=0) {
+			throw new IllegalArgumentException();
+		}
+		leistung = this.leistung;
 	}
 
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	@Override
 	public String toString() {
-		return "Auto [farbe=" + farbe + ", ps=" + ps + ", model=" + model + "]";
+		String farbString = ""+farbe;
+		if (farbe.equals(Color.RED)) {
+			farbString = "rot";
+		}
+		if (farbe.equals(Color.WHITE)) {
+			farbString = "weiß";
+		}
+		if (farbe.equals(Color.BLACK)) {
+			farbString = "rot";
+		}
+		return type + ", " + farbString + ", " + leistung + "PS";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Auto());
 	}
 
 }
--- src/froehlich/Autotest.java
+++ src/froehlich/Autotest.java
@@ -1,32 +1,90 @@
 package froehlich;
 
+import java.awt.Color;
+
 import org.junit.*;
+
+/**
+ * The Class Autotest.
+ */
 public class Autotest {
 
+	/**
+	 * Test 1.
+	 */
 	@Test
-	public void test1(){
-		Auto a = new Auto();
-		a.setPs(123);
+	public void test1() {
+		Auto a = new Auto("Nissan", Color.ORANGE, 198);
+
+		// Testen
+		Assert.assertEquals(160, a.getLeistung(), 0);
+		// a.setFarbe("rot");
+	}
+
+	@Test
+	public void test4() {
+		Auto a = new Auto("Nissan", Color.ORANGE, -10);
+
+		// Testen
+		Assert.assertEquals(10, a.getLeistung(), 0);
+		// a.setFarbe("rot");
+	}
+
+	/**
+	 * Test 2.
+	 */
+	@Test
+	public void test2() {
+		Auto a = new Auto("Nissan", Color.RED, 198);
+		// Testen
+		Assert.assertEquals(Color.red, a.getFarbe());
+	}
+
+	/**
+	 * Test 3.
+	 */
+	@Test
+	public void test3() {
+		Auto a = new Auto("Eiswagen", Color.ORANGE, 198);
+		// Testen
+		Assert.assertEquals("Eiswagen", a.getType());
+
+	}
+
+	@Test
+	public void test5() {
+		Auto a = new Auto("Audi", Color.WHITE, 122130);
+
+		// Testen
+		Assert.assertEquals(10, a.getLeistung(), 0);
+		// a.setFarbe("rot");
+	}
+
+	@Test
+	public void test6() {
+		Auto a = new Auto("Audi", Color.WHITE, 1231);
+
+		// Testen
+		Assert.assertEquals("Test", a.getType(), 0);
+		// a.setFarbe("rot");
+	}
+
+	@Test
+	public void test7() {
+		Auto a = new Auto(" ", null, -10);
+
+		// Testen
+		Assert.assertEquals(null, a.getFarbe(), 0);
+		// a.setFarbe("rot");
+	}
+
+	@Test
+	public void test8(){
+		Auto a = new Auto(null ,null , 0);
+	
 		//Testen
-		Assert.assertEquals(123,a.getPs(),0);
+		Assert.assertEquals(null ,a.getLeistung(),0);
 		//a.setFarbe("rot");
 	}
-	
-	@Test
-	public void test2(){
-		Auto a = new Auto();
-		a.setFarbe("rot");
-		//Testen
-		Assert.assertEquals("rot",a.getFarbe());
-	}
-	
-	@Test
-	public void test3(){
-		Auto a = new Auto("Violett", 156, "Nissan GTR");
-		//Testen
-		Assert.assertEquals("Nissan GTR", a.getModel());
-	}
-	
-	
-	
+
 }
